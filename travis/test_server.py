bfe4b19acf2858fa35fbaf7622e39baa5d0d9728
JoshuaJan <popkar77@gmail.com>
2016-08-12 13:45:42 +0800

    Update test_server.py

--- travis/test_server.py
+++ travis/test_server.py
@@ -210,7 +210,7 @@ def setup_server(db, odoo_unittest, tested_addons, server_path,
                      "--db_user=odoo",
                      "--db_password=odoo",
                      "--db_port=5432",
-                     "--log-level=debug",
+                     "--log-level=debug_sql",
                      "--stop-after-init",
                      "--init", ','.join(preinstall_modules),
                      ] + install_options
